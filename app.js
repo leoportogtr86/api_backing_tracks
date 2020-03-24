fc3671f5f174b54a244b697cdb3abf56858c7738
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-03-24 13:14:57 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let bt = require('./public/files/bt.json')
+
+bt = bt.map(function (e) {
+
+    return e.titulo
+
+})
+
+console.log(bt)
---
+++ scrapping.js
@@ -0,0 +1,14 @@
+var request = require('request')
+var axios = require('axios')
+var cheerio = require('cheerio')
+
+request('https://www.youtube.com/watch?v=0fJ29wqHQyU', function (err, res, body) {
+
+
+
+    const $ = cheerio.load(body)
+
+    console.log($('div').html())
+
+
+})
