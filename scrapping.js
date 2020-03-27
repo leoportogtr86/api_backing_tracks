14f79b07e1169dccd0eac62ff6c40ba76b4fcee8
leoportogtr86 <52595646+leoportogtr86@users.noreply.github.com>
2020-03-27 11:55:04 -0300

    small update

--- scrapping.js
+++ scrapping.js
@@ -1,15 +1,57 @@
+var artist = 'roberto-carlos'
+var music = 'como-grande-meu-amor-por-voce'
+
+
 var request = require('request')
-var axios = require('axios')
 var cheerio = require('cheerio')
-var jquery = require('jquery')
+var url = 'https://www.cifraclub.com.br/' + artist + '/' + music +'/'
+var fs = require('fs')
+var axios = require('axios')
 
-request('https://g1.globo.com/', function (err, res, body) {
+function repertorio (artista) {
 
+    axios.get('https://www.letras.mus.br/' + artista + '/').then(function (res) {
+
+
+        let $ = cheerio.load(res.data)
+
+        let repertorio
+
+        repertorio = $('.-song').text()
+
+        repertorio = repertorio.split('  ')
+
+
+
+        console.log(repertorio)
+
+
+    })
+
+}
+
+
+request(url, function (err, res, body) {
 
 
     const $ = cheerio.load(body)
 
-    console.log($('h2').text())
 
+    let cifra_json = {
 
+        titulo: $('h1.t1').text(),
+        artista: $('h2.t3').text(),
+        tonalidade: $('#cifra_tom a').text(),
+        cifra: $('pre').text(),
+    }
+
+    console.log(cifra_json)
+    fs.writeFile('./public/files/'+cifra_json.titulo+'.json',JSON.stringify(cifra_json), function () {
+
+        console.log('Tá salvo!')
+
+    })
 })
+
+
+//repertorio('titas')
